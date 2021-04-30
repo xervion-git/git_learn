647b798274eacf3926b97d810e4666ca4c8fc668
Xervion <Xervion@Xervion.com>
2021-04-29 19:02:53 -0700

    [EDIT]: sin function added

--- calcFunc.cpp
+++ calcFunc.cpp
@@ -14,3 +14,8 @@ void dev(size_t& res, size_t in1, size_t in2)
 {
     res = in1/in2;
 }
+
+void sin(size_t& res, size_t in1, size_t in2)
+{
+    res = in1&in2;
+}
--- calcFunc.h
+++ calcFunc.h
@@ -5,3 +5,4 @@
 void add(size_t& res, size_t in1, size_t in2);
 void mul(size_t& res, size_t in1, size_t in2);
 void dev(size_t& res, size_t in1, size_t in2);
+void sin(size_t& res, size_t in1, size_t in2);
